0cda8df9fd9a19b68f433dd3444c311e1c9907e0
laban211 <lalaban211@gmail.com>
2018-04-27 08:47:03 +0200

    Cleaned up the code a bit, removed some comments

--- main.go
+++ main.go
@@ -9,17 +9,6 @@ import (
 	"github.com/AsynkronIT/protoactor-go/actor"
 )
 
-// type listRow struct {
-// 	week    int
-// 	user    string
-// 	mon     float32
-// 	tue     float32
-// 	wed     float32
-// 	thu     float32
-// 	fri     float32
-// 	projNum int
-// }
-
 type listRow struct {
 	row string
 }
@@ -31,10 +20,6 @@ var sentCounter int
 
 func (state *messageActor) Receive(context actor.Context) {
 	switch msg := context.Message().(type) {
-	// case *listRow:
-	// 	fmt.Printf("%v \t %v \t %v \t %v \t %v \t %v \t %v \t %v \n",
-	// 		msg.week, msg.user, msg.mon, msg.tue, msg.wed, msg.thu,
-	// 		msg.fri, msg.projNum)
 	case *listRow:
 		receiveCounter++
 		if receiveCounter%50000 == 0 {
@@ -80,29 +65,4 @@ func main() {
 		println("\nNågonting gick fel >:(")
 	}
 
-	// for {
-	// 	line, _, err := reader.ReadLine()
-
-	// 	if err == io.EOF {
-	// 		break
-	// 	}
-
-	// 	pid.Tell(&listRow2{row: string(line)})
-	// }
-
-	//fmt.Println(dat) // Print the content as 'bytes'
-	// str := string(file)
-	// fmt.Println(str) // Print the content as 'string'
-
-	// pid.Tell(&listRow{
-	// 	week:    3,
-	// 	user:    "Daniel",
-	// 	mon:     4,
-	// 	tue:     3,
-	// 	wed:     2,
-	// 	thu:     8,
-	// 	fri:     1,
-	// 	projNum: 1234567890,
-	// })
-	// console.ReadLine()
 }
