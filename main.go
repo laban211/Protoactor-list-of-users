cc0972ff0217e3d525ff7408fbc768d217591dfe
laban211 <lalaban211@gmail.com>
2018-05-03 10:31:34 +0200

    The spawning of actors now works as expected! :)

--- main.go
+++ main.go
@@ -20,33 +20,13 @@ type userManager struct{}
 type projectActor struct{}
 type userActor struct{}
 
-// type messageActor struct {
-// 	localCounter int
-// }
-
 type askForRows struct {
 	test int
 }
 
 var sentCounter int
-var createdKeys int
-var receivedCounter int
-
-// func (state *messageActor) Receive(context actor.Context) {
-// 	switch msg := context.Message().(type) {
-// 	case *listRow:
-// 		state.localCounter++
-// 		if state.localCounter > 100 {
-// 			fmt.Printf("\n%v\n", msg.row)
-// 		}
-// 	case *askForRows:
-// 		// fmt.Println("hej")
-// 		context.Respond(state.localCounter)
-// 	}
-// }
 
 func (state *mainRouter) Receive(context actor.Context) {
-	//Hur deklarerar jag en actor?
 	projects := actor.FromProducer(newProjectActor)
 	users := actor.FromProducer(newUserActor)
 	childActors := make(map[string]*actor.PID)
@@ -65,7 +45,6 @@ func (state *mainRouter) Receive(context actor.Context) {
 
 	switch msg := context.Message().(type) {
 	case *listRow:
-		//tell them what to do
 		childActors["projects"].Tell(msg)
 		childActors["users"].Tell(msg)
 	default:
@@ -78,7 +57,6 @@ func (state *projectManager) Receive(context actor.Context) {
 	actors := make(map[string]*actor.PID)
 	switch msg := context.Message().(type) {
 	case *listRow:
-		//Split
 		projNum := strings.Split(msg.row, ",")[7]
 		foundActor, ok := actors[projNum]
 		if !ok {
@@ -90,7 +68,7 @@ func (state *projectManager) Receive(context actor.Context) {
 }
 
 func (state *projectActor) Receive(context actor.Context) {
-
+	println("HI")
 }
 
 func (state *userManager) Receive(context actor.Context) {
@@ -98,7 +76,6 @@ func (state *userManager) Receive(context actor.Context) {
 	users := make(map[string]*actor.PID)
 	switch msg := context.Message().(type) {
 	case *listRow:
-		//Split
 		userID := strings.Split(msg.row, ",")[1]
 		foundUser, ok := users[userID]
 		if !ok {
@@ -110,7 +87,7 @@ func (state *userManager) Receive(context actor.Context) {
 }
 
 func (state *userActor) Receive(context actor.Context) {
-
+	println("Ho")
 }
 
 func check(e error) {
@@ -135,12 +112,8 @@ func main() {
 	println("Skickar...\n")
 
 	// Create an actor
-	// props := actor.FromProducer(func() actor.Actor { return &messageActor{} })
 	props := actor.FromProducer(newParentActor)
 	pid := actor.Spawn(props)
-
-	// A map for storing actors
-	// hash := make(map[string]*actor.PID)
 
 	// Reading from file
 	file, err := os.Open("text.csv")
@@ -151,30 +124,14 @@ func main() {
 	scanner := bufio.NewScanner(file)
 
 	for scanner.Scan() {
-		// projNum := strings.Split(scanner.Text(), ",")[7]
 
-		//if not exist
-		// foundActor, ok := hash[projNum]
-		// if !ok {
-		// 	createdKeys++
-		// 	foundActor = actor.Spawn(props)
-		// 	hash[projNum] = foundActor
-		// }
-
-		// foundActor.Tell(&listRow{row: scanner.Text()})
 		pid.Tell(&listRow{row: scanner.Text()})
 		sentCounter++
 	}
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
-	// for _, value := range hash {
-	// 	result, _ := value.RequestFuture(&askForRows{test: 1}, 30*time.Second).Result() // await result
-	// 	receivedCounter += result.(int)
-	// }
 
 	fmt.Println("Allt är skickat!")
 	// fmt.Scanln()
-	// fmt.Printf("Sent: %v\nCreated keys: %v\nReceived: %v",
-	// 	sentCounter, createdKeys, receivedCounter)
 }
