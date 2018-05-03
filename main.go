c2503216a49506a2c1e283eea2787236bf784225
laban211 <lalaban211@gmail.com>
2018-05-03 09:39:33 +0200

    I believe the main-actor works pretty much as expected now, still build error though

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strings"
 	"time"
 
 	"github.com/AsynkronIT/protoactor-go/actor"
@@ -15,9 +14,13 @@ type listRow struct {
 	row string
 }
 
-type messageActor struct {
-	localCounter int
-}
+type mainActor struct{}
+type projectManager struct{}
+type userManager struct{}
+
+// type messageActor struct {
+// 	localCounter int
+// }
 
 type askForRows struct {
 	test int
@@ -27,17 +30,53 @@ var sentCounter int
 var createdKeys int
 var receivedCounter int
 
-func (state *messageActor) Receive(context actor.Context) {
+// func (state *messageActor) Receive(context actor.Context) {
+// 	switch msg := context.Message().(type) {
+// 	case *listRow:
+// 		state.localCounter++
+// 		if state.localCounter > 100 {
+// 			fmt.Printf("\n%v\n", msg.row)
+// 		}
+// 	case *askForRows:
+// 		// fmt.Println("hej")
+// 		context.Respond(state.localCounter)
+// 	}
+// }
+
+func (state *mainActor) Receive(context actor.Context) {
+	//Hur deklarerar jag en actor?
+	project := actor.FromProducer(newProjectActor)
+	user := actor.FromProducer(newUserActor)
+	childActors := make(map[string]*actor.PID)
+
+	foundProjectActor, ok := childActors["project"]
+	if !ok {
+		foundProjectActor = actor.Spawn(project)
+		childActors["project"] = foundProjectActor
+	}
+
+	foundUserActor, ok := childActors["user"]
+	if !ok {
+		foundUserActor = actor.Spawn(user)
+		childActors["user"] = foundUserActor
+	}
+
 	switch msg := context.Message().(type) {
 	case *listRow:
-		state.localCounter++
-		if state.localCounter > 100 {
-			fmt.Printf("\n%v\n", msg.row)
-		}
-	case *askForRows:
-		// fmt.Println("hej")
-		context.Respond(state.localCounter)
+		//tell them what to do
+		childActors["project"].Tell(msg)
+		childActors["user"].Tell(msg)
+	default:
+		fmt.Print("Something went wrong >:(")
 	}
+}
+
+func (state *projectManager) Receive(context actor.Context) {
+
+}
+
+func (state *userManager) Receive(context actor.Context) {
+
 }
 
 func check(e error) {
@@ -46,14 +85,28 @@ func check(e error) {
 	}
 }
 
+func newParentActor() actor.Actor {
+	return &mainActor{}
+}
+
+func newProjectActor() actor.Actor {
+	return &projectManager{}
+}
+
+func newUserActor() actor.Actor {
+	return &userManager{}
+}
+
 func main() {
 	println("Skickar...\n")
 
 	// Create an actor
-	props := actor.FromProducer(func() actor.Actor { return &messageActor{} })
+	// props := actor.FromProducer(func() actor.Actor { return &messageActor{} })
+	props := actor.FromProducer(func() actor.Actor { return &mainActor{} })
+	pid := actor.Spawn(props)
 
 	// A map for storing actors
-	hash := make(map[string]*actor.PID)
+	// hash := make(map[string]*actor.PID)
 
 	// Reading from file
 	file, err := os.Open("text.csv")
@@ -64,18 +117,18 @@ func main() {
 	scanner := bufio.NewScanner(file)
 
 	for scanner.Scan() {
-		projNum := strings.Split(scanner.Text(), ",")[7]
+		// projNum := strings.Split(scanner.Text(), ",")[7]
 
 		//if not exist
-		value, ok := hash[projNum]
-		if !ok {
-			createdKeys++
-			value = actor.Spawn(props)
-			hash[projNum] = value
-		}
+		// foundActor, ok := hash[projNum]
+		// if !ok {
+		// 	createdKeys++
+		// 	foundActor = actor.Spawn(props)
+		// 	hash[projNum] = foundActor
+		// }
 
-		value.Tell(&listRow{row: scanner.Text()})
-
+		// foundActor.Tell(&listRow{row: scanner.Text()})
+		pid.Tell(&listRow{row: scanner.Text()})
 		sentCounter++
 	}
 	if err := scanner.Err(); err != nil {
